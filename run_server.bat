0a88158a96fb843a59bd7c6cfdba30c7a2250b49
vitaly - git <vitaly@secthor.com>
2016-08-13 11:44:47 +0300

    new music folder + better scripts

--- run_server.bat
+++ run_server.bat
@@ -3,6 +3,7 @@ cd /d %~dp0
 docker run -it -p 80:80 -p 22123:22 ^
 -v "%cd%\ssh-key\authorized_keys":/root/.ssh/authorized_keys:ro ^
 -v "%cd%\beets-data":/radio-stream/data ^
+-v "%cd%\new-music":/radio-stream/new-music ^
 -v "%cd%\scripts\bundled":/radio-stream/scripts/bundled ^
 -v "%cd%\scripts\user":/radio-stream/scripts/user ^
 -v "%userprofile%\Music\beets-music":/radio-stream/music ^
