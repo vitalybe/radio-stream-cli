9f9b2406b3c8b42708eb3f08b0873191d8b088ec
vitaly - git <vitaly@secthor.com>
2016-11-01 22:25:45 +0200

    splitting config files to source and user

---
+++ config/readme.md
@@ -0,0 +1,3 @@
+To override the default config `config_source.yaml` create a file here: `config_user.yaml`.
+
+See more in documentation.
--- run_server.sh
+++ run_server.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
+
+# get this script folder
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# this is required since docker on windows wants Windows-style directories 
+# TODO - Run only on Windows
+type cygpath >/dev/null 2>&1 && DIR=$(cygpath -w "$DIR")
+
 while getopts "hp:d" opt; do
   case $opt in
     h)
@@ -13,7 +21,7 @@ while getopts "hp:d" opt; do
       ;;
     p)
       NGINX_PASSWORD=$OPTARG
-      echo "Setting password: $NGINX_PASSWORD"
+      echo "Setting password: $NGINX_PASSWORD" 
   esac
 done
 
@@ -24,18 +32,14 @@ fi
 # make $@ contain all the other arguments
 shift $((OPTIND-1))
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-# this is required since docker on windows wants Windows-style directories 
-type cygpath >/dev/null 2>&1 && DIR=$(cygpath -w "$DIR")
-
 docker run -it -p 80:80  -p 22123:22\
     -v "$DIR\data\ssh-key\authorized_keys":/root/.ssh/authorized_keys:ro\
     -v "$DIR\data\beets":/radio-stream/data/beets\
     -v "$DIR\new-music":/radio-stream/new-music\
     -v "$DIR\scripts\bundled":/radio-stream/scripts/bundled\
     -v "$DIR\scripts\user":/radio-stream/scripts/user\
+    -v "$DIR\config":/radio-stream/config\
     -v "C:\Users\htpc2\Music\beets-music":/radio-stream/music\
     -e "NGINX_PASSWORD=$NGINX_PASSWORD"\
     $DEV_MODE\
-    vitalybe/radio-stream $@ # provide all the arguments except the first
+    vitalybe/radio-stream
