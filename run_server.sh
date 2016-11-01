5e5242a7905a6b34496f929819e7728de93cd711
vitaly - git <vitaly@secthor.com>
2016-11-01 22:11:14 +0200

    proper options usage in run_server.sh

--- run_server.sh
+++ run_server.sh
@@ -1,13 +1,33 @@
 #!/usr/bin/env bash
-if [ -z $1 ]; then echo "Usage: ./run_server.sh Password1234" && exit 1; fi
+while getopts "hp:d" opt; do
+  case $opt in
+    h)
+      echo "Usage: ./run_server.sh [-p password] [-d]" >&2
+      echo "-p - Set radio-stream password" >&2
+      echo "-d - Development mode" >&2
+      exit 0
+      ;;
+    d)
+      echo "*** Running in development mode ***"
+      DEV_MODE="-v $DIR\..\beets:/radio-stream/beets"
+      ;;
+    p)
+      NGINX_PASSWORD=$OPTARG
+      echo "Setting password: $NGINX_PASSWORD"
+  esac
+done
+
+if [ -z $NGINX_PASSWORD ]; then
+    echo "*** WARNING: PASSWORD NOT SET - USING DEFAULT ***" && NGINX_PASSWORD=radio
+fi
+
+# make $@ contain all the other arguments
+shift $((OPTIND-1))
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# this is required since docker on windows wants Windows-style directories 
 type cygpath >/dev/null 2>&1 && DIR=$(cygpath -w "$DIR")
-NGINX_PASSWORD=$1
-
-DEV_MODE=""
-# DEV_MODE="-v $DIR\..\beets:/radio-stream/beets"
 
 docker run -it -p 80:80  -p 22123:22\
     -v "$DIR\data\ssh-key\authorized_keys":/root/.ssh/authorized_keys:ro\
@@ -18,4 +38,4 @@ docker run -it -p 80:80  -p 22123:22\
     -v "C:\Users\htpc2\Music\beets-music":/radio-stream/music\
     -e "NGINX_PASSWORD=$NGINX_PASSWORD"\
     $DEV_MODE\
-	vitalybe/radio-stream ${@:2} # provide all the arguments except the first
+    vitalybe/radio-stream $@ # provide all the arguments except the first
